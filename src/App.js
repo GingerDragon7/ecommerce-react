4dd32ef77b0c427f2a5809e51d2d696b5ae6b7b0
Sunny Bhadani <sunnybhadani898@gmail.com>
2022-01-23 23:36:30 +0530

    Setup the Header Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StorefrontIcon from '@mui/icons-material/Storefront';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header_logo">
+      <StorefrontIcon className="header__logoImage" fontSize='large'/>
+      <h2 className="header__logoTitle">eShop</h2>
+      </div>
+      <div className="header__search">
+
+      </div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <div className="nav__item">
+          <ShoppingBasketIcon />
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+
+      </div>
     </div>
   );
 }
